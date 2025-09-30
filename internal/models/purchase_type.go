e281fc930d6f47a350c82c4ba267e3d79fcb7917
Roger Toledo <roger.toledo@olxbr.com>
2025-09-30 09:35:58 -0300

    feat: add crud purchase type

---
+++ internal/models/purchase_type.go
@@ -0,0 +1,39 @@
+package models
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/google/uuid"
+)
+
+type PurchaseType struct {
+	ID   uuid.UUID `json:"id"`
+	Name string    `json:"name"`
+}
+
+func (pt *PurchaseType) Validate(removeID bool) error {
+	var invalidFields []string
+
+	if !removeID {
+		if pt.ID == uuid.Nil {
+			invalidFields = append(invalidFields, "ID")
+		}
+	}
+	
+	if pt.Name == "" {
+		invalidFields = append(invalidFields, "Name")
+	}
+	
+	if len(invalidFields) > 0 {
+		fields := strings.Join(invalidFields, ", ")
+
+		if len(invalidFields) == 1 {
+			return fmt.Errorf("the field %s is required", fields)
+		}		
+
+		return fmt.Errorf("the fields %s are required", fields)
+	}
+
+	return nil
+}
---
+++ internal/service/purchase_type.go
@@ -0,0 +1,67 @@
+package service
+
+import (
+	"github.com/google/uuid"
+	"github.com/me/finance/internal/models"
+	"github.com/me/finance/internal/repository"
+)
+
+type PurchaseTypeUseCase interface {
+	CreatePurchaseType(pt models.PurchaseType) error
+	UpdatePurchaseType(pt models.PurchaseType) error
+	DeletePurchaseType(id uuid.UUID) error
+	FindPurchaseTypeByID(id uuid.UUID) (models.PurchaseType, error)
+	FindAllPurchaseTypes() ([]models.PurchaseType, error)
+}
+
+type PurchaseType struct {
+	repository repository.RepositoryPurchaseType
+}
+
+func NewPurchaseTypeUseCase(r repository.RepositoryPurchaseType) PurchaseTypeUseCase {
+	return &PurchaseType{
+		repository: r,
+	}
+}
+
+func (p *PurchaseType) CreatePurchaseType(pt models.PurchaseType) error {
+	if err := p.repository.Create(pt); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *PurchaseType) UpdatePurchaseType(pt models.PurchaseType) error {
+	if err := p.repository.Update(pt); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *PurchaseType) DeletePurchaseType(id uuid.UUID) error {
+	if err := p.repository.Delete(id); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *PurchaseType) FindPurchaseTypeByID(id uuid.UUID) (models.PurchaseType, error) {
+	purchaseType, err := p.repository.FindByID(id)
+	if err != nil {
+		return models.PurchaseType{}, err
+	}
+
+	return purchaseType, nil
+}
+
+func (p *PurchaseType) FindAllPurchaseTypes() ([]models.PurchaseType, error) {
+	purchaseTypes, err := p.repository.FindAll()
+	if err != nil {
+		return []models.PurchaseType{}, err
+	}
+
+	return purchaseTypes, nil
+}
